5e924f45f3302796ec4a9330c5fa1be0bce9eca9
Onojeta Brown <onosbrown.saved@gmail.com>
2023-10-19 11:35:57 +0100

    updated

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,18 +1,29 @@
-#!/bin/bash 
+#!/bin/sh
 set -e
 
 init() {
-    cp .env.example .env
-    php artisan key:generate
-    composer install \
-        --no-interaction \
-        --no-plugins \
-        --no-scripts \
-        --no-dev \
-        --prefer-dist
+    if [ ! -f ".env" ]; then
+        cp .env.example .env
+        php artisan key:generate
+    fi
 
-    composer dump-autoload --no-scripts
-    php-fpm
+    if [ ! -f "vendor/autoload.php" ]; then
+        composer install \
+            --no-interaction \
+            --no-plugins \
+            --no-scripts \
+            --no-dev \
+            --prefer-dist
+        
+        composer dump-autoload --no-scripts
+        
+        find . -type f -exec chmod 644 {} \;
+        find . -type d -exec chmod 775 {} \;
+        chown -R www-data:root ./ 
+        chmod -R 777 storage
+        chmod -R 777 bootstrap/cache/
+    fi
 }
 
 init
+php-fpm
